7588794d86eb6dd087338e0d687a9617aca7043e
Akhil Rajan <rajanakhil07@gmail.com>
2026-03-19 11:48:43 +0530

    fix(booking-service): remove un-necessary things

--- booking-service/src/main/java/com/ticketblitz/booking/service/DistributedLockService.java
+++ booking-service/src/main/java/com/ticketblitz/booking/service/DistributedLockService.java
@@ -11,7 +11,7 @@ import java.util.function.Supplier;
 
 
 /**
- * Distributed Lock Service - STAFF ENGINEER PATTERN
+ * Distributed Lock Service
  *
  * DESIGN PATTERN: Facade Pattern
  * ===============================
